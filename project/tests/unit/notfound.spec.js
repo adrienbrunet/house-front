086be9d1f19548a5a2bd4f363fc8c630a3895716
Adrien Brunet <adrien@emencia.com>
2018-10-28 19:19:10 +0100

    [NEW] :white_check_mark: Adding test for NotFound View

---
+++ project/tests/unit/notfound.spec.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import {
+  mount
+} from '@vue/test-utils';
+import Vuetify from 'vuetify';
+import NotFound from '@/views/NotFound.vue';
+
+describe('NotFound.vue', function () {
+  let wrp;
+
+  beforeEach(() => {
+    Vue.use(Vuetify);
+    wrp = mount(NotFound);
+  });
+
+  it("display content not found message", () => {
+    const msg = "Content Not Found";
+    expect(wrp.text()).toMatch(msg);
+  });
+});
