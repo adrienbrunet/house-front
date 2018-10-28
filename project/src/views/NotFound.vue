028c9f441cf14742f67b482b647c0fd3fba51f3e
Adrien Brunet <adrien@emencia.com>
2018-10-28 18:49:45 +0100

    [CHG] Add not Found Page

---
+++ project/src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+<v-container fluid>
+    <v-slide-y-transition mode="out-in">
+        <div>
+            <h1>Content Not Found</h1>
+            <p>Sorry, not sorry. This page can't be found.</p>
+        </div>
+    </v-slide-y-transition>
+</v-container>
+</template>
