954954287f78d5075de917fe70e9322088468577
Adrien Brunet <adrien@emencia.com>
2018-10-28 19:19:51 +0100

    [CHG] Update made running npm run lint

--- project/src/views/NotFound.vue
+++ project/src/views/NotFound.vue
@@ -1,10 +1,10 @@
 <template>
-<v-container fluid>
+  <v-container fluid>
     <v-slide-y-transition mode="out-in">
-        <div>
-            <h1>Content Not Found</h1>
-            <p>Sorry, not sorry. This page can't be found.</p>
-        </div>
+      <div>
+        <h1>Content Not Found</h1>
+        <p>Sorry, not sorry. This page can't be found.</p>
+      </div>
     </v-slide-y-transition>
-</v-container>
+  </v-container>
 </template>
