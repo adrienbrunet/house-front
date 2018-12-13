a5cc1e420513815c02ecb26d1edf19f7dc66d3f1
Adrien Brunet <adrien@emencia.com>
2018-12-14 00:25:45 +0100

    chg: lint fix, unused var

--- src/components/forms/SignUpForm.vue
+++ src/components/forms/SignUpForm.vue
@@ -64,7 +64,7 @@ export default {
       "resetPasswordErrors",
       "setErrorPasswordsMatch"
     ]),
-    resetEmailErrorsRule(v) {
+    resetEmailErrorsRule() {
       this.resetEmailErrors();
       return true;
     },
